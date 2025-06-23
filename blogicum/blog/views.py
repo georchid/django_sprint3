4b3b44b8c56775f5ca2c8c76ffdead9fc20351f8
georchid <cherngeorgevvv@gmail.com>
2025-06-23 16:35:05 +0300

    fix pep8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post, Category
-from datetime import datetime
 from django.utils import timezone
 
 
